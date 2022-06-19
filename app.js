441e23c444f80b645e851f749752467f6c26993e
kristaver <kristaver@gmail.com>
2022-06-19 16:35:18 +0200

    added express server....

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+app.use(express.static('src'));
+
+app.get('/', (req, res) => {
+  res.render('index.html');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
